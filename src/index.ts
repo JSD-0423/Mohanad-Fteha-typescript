d87ad63d9dd3c0edcc9986462b32d1c9279335b7
mohanadft <mohanadfteha@gmail.com>
2023-06-04 19:29:22 +0300

    Handle errors

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,29 @@
-import express, { Request, Response, Express } from "express"
+import express, { Request, Response, Express, NextFunction } from "express"
 import { readJSONFile } from "./utils/fileUtils.js"
 const app: Express = express()
 const PORT = 3000
 
 
-app.get("/books", (req: Request, res: Response) => {
-  let startsWith = '';
+app.get("/books", (req: Request, res: Response, next: NextFunction) => {
 
-  if (req.query?.startsWith) {
-    startsWith = String(req.query.startsWith)
+  try {
+    let startsWith = '';
+
+    if (req.query?.startsWith) {
+      startsWith = String(req.query.startsWith)
+    }
+
+    const { data: books } = readJSONFile('../data/books.json')
+
+    res.status(200).json(books.filter(book => book.name.startsWith(startsWith)))
+  } catch (e) {
+    next(e);
   }
-
-  const { data: books } = readJSONFile('../data/books.json')
-
-  res.status(200).json(books.filter(book => book?.name?.startsWith(startsWith)))
 })
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ error: err.message });
+});
 
 app.listen(PORT, () => {
   console.log(`The server is running on port ${PORT}`);
